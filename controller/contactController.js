2e8724b744714ff8f7b76dd4060e7ca5f02ad556
Joysjo1 <joysjobin70@gmail.com>
2024-05-13 22:03:35 +0530

    change : middle ware file changes

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,46 +1,48 @@
+const asyncHandler = require("express-async-handler")
+
 // @desc Get all Contacts
 // route GET /api/contacts
 // @access Public
 
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "get all contacts" })
-}
+})
 
 
 // @desc Get  Contact
 // route GET /api/contact/:id
 // @access Public
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "get contact" })
-}
+})
 
 // @desc  create New Contact
 // route POST /api/contacts
 // @access Public
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     // const { } = req.body
     res.status(201).json({ message: req.body })
-}
+})
 
 
 // @desc update Conatct
 // route PUT /api/contacts/:id
 // @access Public
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `update Contact${req.params.id}` })
-}
+})
 
 
 // @desc delete Contacts
 // route DELETE /api/contacts/:id
 // @access Public
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `delete contact ${req.params.id}` })
-}
+})
 
 
 module.exports = { getContacts, getContact, createContact, updateContact, deleteContact }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorhandler");
 const dotenv = require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+app.use(errorHandler)
 const port = process.env.PORT || 5000;
 app.use('/api/contacts', require('../mycontacts-backend/routes/contactRoutes'));
 
