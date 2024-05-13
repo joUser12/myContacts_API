b65f983f1b23ce663f1d30829427168b2e52a6a1
Joysjo1 <joysjobin70@gmail.com>
2024-05-13 21:10:16 +0530

    changes : API crud basic changes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { getContacts, getContact, createContact, updateContact, deleteContact } = require("../controller/contactController")
 
-router.route("").get ((req, res) => {
-    res.status(200).json({ message: "get all contacts" })
-})
+router.route("").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 
 module.exports = router;
