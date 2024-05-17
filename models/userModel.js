91d99d296070b193d3c5a5fb883bb52bdd55e496
Joysjo1 <joysjobin70@gmail.com>
2024-05-17 06:57:40 +0530

    fix

--- models/userModel.js
+++ models/userModel.js
@@ -20,4 +20,26 @@ const userSchema = mongoose.Schema({
     });
 
 
-module.exports = mongoose.model("User",userSchema);
+module.export= mongoose.model("User",userSchema);const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please add the user name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add the user email address"],
+        unique: [true, "Email  address already taken"]
+    },
+    password: {
+        type: String,
+        required: [true, "please add the user password"]
+    },
+}
+    , {
+        timeStamps: true
+    });
+
+
+module.export= mongoose.model("User",userSchema);
