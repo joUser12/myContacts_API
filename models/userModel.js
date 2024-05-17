907b901fd898043200dc674207abc03a750e3437
Joysjo1 <joysjobin70@gmail.com>
2024-05-17 06:59:41 +0530

    g

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,3 @@
-const mongoose = require("mongoose");
-
-const userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Please add the user name"]
-    },
-    email: {
-        type: String,
-        required: [true, "Please add the user email address"],
-        unique: [true, "Email  address already taken"]
-    },
-    password: {
-        type: String,
-        required: [true, "please add the user password"]
-    },
-}
-    , {
-        timeStamps: true
-    });
-
-
-module.export= mongoose.model("User",userSchema);const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     username: {
