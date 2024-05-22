10dfc6ab17929ee28b996a59dba927eb84148acd
Joysjo1 <joysjobin70@gmail.com>
2024-05-22 22:32:04 +0530

    changes

--- controller/contactController.js
+++ controller/contactController.js
@@ -3,17 +3,17 @@ const Contact = require("../models/contactModel")
 
 // @desc Get all Contacts
 // route GET /api/contacts
-// @access Public
+// @access private
 
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find();
+    const contacts = await Contact.find({ user_id: req.user.id });
     res.status(200).json(contacts)
 })
 
 
 // @desc Get  Contact
 // route GET /api/contact/:id
-// @access Public
+// @access private
 
 const getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
@@ -28,7 +28,7 @@ const getContact = asyncHandler(async (req, res) => {
 
 // @desc  create New Contact
 // route POST /api/contacts
-// @access Public
+// @access private
 
 const createContact = asyncHandler(async (req, res) => {
     const { name, email, phone } = req.body;
@@ -37,16 +37,17 @@ const createContact = asyncHandler(async (req, res) => {
         throw new Error("All files are mandatory");
     }
 
-    const contact = await Contact.create({
-        name, email, phone
-    })
+    const contact = await Contact.create(
+        {
+            name, email, phone,user_id:req.user.id
+        })
     res.status(201).json(contact)
 })
 
 
 // @desc update Conatct
 // route PUT /api/contacts/:id
-// @access Public
+// @access private
 
 const updateContact = asyncHandler(async (req, res) => {
     console.log(req.params)
@@ -54,6 +55,12 @@ const updateContact = asyncHandler(async (req, res) => {
     console.log(req.body)
     if (!contact) {
         throw new Error("Contact not found")
+    }
+
+    if(contact.user._id.toString()!==req.user.id){
+        res.status(403);
+        throw new Error("user don't have permission to update other user contact")
+
     }
     const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id,
@@ -67,7 +74,7 @@ const updateContact = asyncHandler(async (req, res) => {
 
 // @desc delete Contacts
 // route DELETE /api/contacts/:id
-// @access Public
+// @access private
 
 const deleteContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
