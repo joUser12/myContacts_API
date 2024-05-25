2e82d189595abce148fff1a7c0c5b38cce5453e1
Joysjo1 <joysjobin70@gmail.com>
2024-05-25 12:25:10 +0530

    ty

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const app = express();
 connectDb();// 
 app.use(express.json());
 app.use(errorHandler)
-const port = process.env.PORT || 5000;
+
 app.use('/api/contacts', require('./routes/contactRoutes.js'));
 app.use('/api/users', require('./routes/userRoutes.js'));
 
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
 });
