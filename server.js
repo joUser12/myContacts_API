6eb409f4c4110b5cfeb11804ceb24524574bd26f
Joysjo1 <joysjobin70@gmail.com>
2024-05-25 12:21:16 +0530

    v

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
-// const errorHandler = require("./middleware/errorhandler");
+const errorHandler = require("./middleware/errorHandler.js");
 const connectDb = require("./config/dbConnection");
 const dotenv = require('dotenv').config();
 
 const app = express();
 connectDb();// 
 app.use(express.json());
-// app.use(errorHandler)
+app.use(errorHandler)
 const port = process.env.PORT || 5000;
-app.use('/api/contacts', require('../mycontacts-backend/routes/contactRoutes'));
-app.use('/api/users', require('../mycontacts-backend/routes/userRoutes'));
+app.use('/api/contacts', require('./routes/contactRoutes.js'));
+app.use('/api/users', require('./routes/userRoutes.js'));
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
