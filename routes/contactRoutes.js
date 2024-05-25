c31966e01a472b9ad008db867db0c3cf781a4569
Joysjo1 <joysjobin70@gmail.com>
2024-05-25 12:29:18 +0530

    fgfgf

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getContacts, getContact, createContact, updateContact, deleteContact } = require("../controller/contactController");
 const validateToken = require("../middleware/validateTokenHandler");
 
-// router.use(validateToken);
+router.use(validateToken);
 router.route("").get(getContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ router.post('/register', registerUser)
 
 router.post('/login', loginUser)
 
-// router.get('/current', validateToken, currentUser)
-router.get('/current', currentUser)
+router.get('/current', validateToken, currentUser)
+// router.get('/current', currentUser)
 
 module.exports = router;
