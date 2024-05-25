63e0a1fd01b0b8d2ac1dcbe8f9893998ab7b26fa
Joysjo1 <joysjobin70@gmail.com>
2024-05-25 18:24:05 +0530

    changes:response changes

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(errorHandler)
 app.use('/api/contacts', require('./routes/contactRoutes.js'));
 app.use('/api/users', require('./routes/userRoutes.js'));
 
+app.get('/api/01', (req, res) => {
+    res.send('Hello, World!');
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
